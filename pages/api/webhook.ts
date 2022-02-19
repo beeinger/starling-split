7b1bb95383c5dbd802d0edfc7a36e3c34d9a9c24
Maciej Sulecki <sulecki.maciek@gmail.com>
2022-02-19 17:25:06 +0000

    fixed webhook endpoint

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -11,15 +11,15 @@ const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
         // Iterates over each entry - there may be multiple if batched
         await Promise.all(body.entry.map(handleMessage));
         // Returns a '200 OK' response to all requests
-        res.status(200).send("EVENT_RECEIVED");
+        return res.status(200).send("EVENT_RECEIVED");
       } else {
         // Returns a '404 Not Found' if event is not from a page subscription
-        res.status(404).send("ERROR_RECEIVING_EVENT");
+        return res.status(404).send("ERROR_RECEIVING_EVENT");
       }
     } catch (error) {
       console.log("PROBABLY_PAGE_ACCESS_TOKEN_EXPIRED");
       console.log(error);
-      res
+      return res
         .status(500)
         .json({ message: "PROBABLY_PAGE_ACCESS_TOKEN_EXPIRED", error });
     }
@@ -38,14 +38,14 @@ const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
       if (mode === "subscribe" && token === VERIFY_TOKEN) {
         // Responds with the challenge token from the request
         console.log("WEBHOOK_VERIFIED");
-        res.status(200).send(challenge);
+        return res.status(200).send(challenge);
       }
     }
 
     console.log("WEBHOOK_NOT_VERIFIED");
     // Responds with '403 Forbidden' if verify tokens do not match
-    res.status(403).send("WEBHOOK_NOT_VERIFIED");
-  } else res.status(404).send("NOT_FOUND");
+    return res.status(403).send("WEBHOOK_NOT_VERIFIED");
+  } else return res.status(404).send("NOT_FOUND");
 };
 
 export default webhook;
